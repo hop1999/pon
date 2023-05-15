b8f3a220e940eecffe28dea776e692e8cd19ae9b
hop1999 <cosmik9@mail.ru>
2023-05-15 20:24:08 +0300

    verim
    verim

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,17 +6,19 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import jm.task.core.jdbc.util.Util;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {  // запомнить DDL I DML
     private final SessionFactory sessionFactory = Util.getSessionFactory();
-    private Transaction transaction = null; // по идее она не статичная, значит с многопоточкой проблем не должно возникнуть.ошибку кстати понял.реально позорная... но у меня мозг тогда не соображал
+
 
     @Override
     public void createUsersTable() {
+        Transaction transaction;
         try (Session session = sessionFactory.openSession()) {
-             transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery(
                             "CREATE TABLE IF NOT EXISTS user (" +
                                     "id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
@@ -33,6 +35,7 @@ public class UserDaoHibernateImpl implements UserDao {  // запомнить DD
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
@@ -44,49 +47,59 @@ public class UserDaoHibernateImpl implements UserDao {  // запомнить DD
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createQuery("DELETE User WHERE id =:id");
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE User WHERE id =:id").setParameter("id", id).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            // можно было через If проверить,
+            // но я не зная что такое assert нажал alt shift enter,
+            // надеюсь не придется из за этого переделывать,зато если придется переделывать
+            //выучу что такое assert 
+            assert transaction != null;
             transaction.rollback();
         }
     }
 
     @Override
     public void saveUser(String name, String last_Name, byte age) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.save(new User(name, last_Name, age));
             transaction.commit();
         } catch (Exception e) {
+            assert transaction != null;
             transaction.rollback();
-            }
         }
+    }
 
 
     @Override
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction(); // <- Может потребоватся в многопоточной среде
             Query<User> query = session.createQuery("FROM User", User.class);
             userList = query.list();
             transaction.commit();
         } catch (Exception e) {
-            transaction.rollback();
-            }
+            e.printStackTrace();
+        }
         return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createQuery("delete from User").executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            assert transaction != null;
             transaction.rollback();
-            }
         }
     }
+}
